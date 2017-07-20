ad4c9762510b4166e2bc7297cfdaaf05d1f221e7
Piotr Lawniczek <piotr.lawniczek@ericsson.com>
2017-07-20 20:00:35 +0200

    findallSurveys, addSurvey methods

--- src/main/java/pl/lawniczek/controller/SurveyController.java
+++ src/main/java/pl/lawniczek/controller/SurveyController.java
@@ -1,6 +1,16 @@
 package pl.lawniczek.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lawniczek.entity.Survey;
+import pl.lawniczek.entity.request.AddSurveyRequest;
+import pl.lawniczek.repository.SurveyRepository;
+
+import java.util.List;
+
 
 /**
  * Created by elawpio on 2017-07-20.
@@ -9,6 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SurveyController {
 
+    private SurveyRepository surveyRepository;
+
+    @Autowired
+    public SurveyController(SurveyRepository surveyRepository) {
+        this.surveyRepository = surveyRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Survey> findAllSurveys(){
+        return surveyRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addSurvey (@RequestBody AddSurveyRequest addSurveyRequest){
+
+        Survey survey = new Survey();
+        survey.setName(addSurveyRequest.getName());
+        surveyRepository.save(survey);
+    }
 
 
 
