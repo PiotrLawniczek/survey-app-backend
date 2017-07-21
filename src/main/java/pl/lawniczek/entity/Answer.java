72ac2a54f04a2d59341fcc388e52eeec0e40323b
Piotr Lawniczek <piotr.lawniczek93@gmail.com>
2017-07-21 13:26:09 +0200

    fix again

--- src/main/java/pl/lawniczek/entity/Answer.java
+++ src/main/java/pl/lawniczek/entity/Answer.java
@@ -12,11 +12,33 @@ public class Answer {
 
     @Id
     @GeneratedValue
-    @ManyToOne
     private long id;
 
+    @ManyToOne
+    private Question question;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", question=" + question +
+                '}';
+    }
 
 }
--- src/main/java/pl/lawniczek/entity/request/AddSurveyRequest.java
+++ src/main/java/pl/lawniczek/entity/request/AddSurveyRequest.java
@@ -5,15 +5,15 @@ package pl.lawniczek.entity.request;
  */
 public class AddSurveyRequest {
 
-    private String name;
+    private String title;
 
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
 
