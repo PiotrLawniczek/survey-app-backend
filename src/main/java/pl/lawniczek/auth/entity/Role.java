91c262fb7dbcc8aa41a9229627851c4dbaa923f5
Piotr Lawniczek <piotr.lawniczek@ericsson.com>
2017-08-29 21:16:45 +0200

    auth - Role entity

---
+++ src/main/java/pl/lawniczek/auth/entity/Role.java
@@ -0,0 +1,47 @@
+package pl.lawniczek.auth.entity;
+
+import javax.jws.soap.SOAPBinding;
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by elawpio on 2017-08-29.
+ */
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
